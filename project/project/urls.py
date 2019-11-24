605cfb85c4a9a93dfd9e7396721986ba12fd4d08
Rosario <Rosario@rosario-mbp.attlocal.net>
2019-11-23 16:57:27 -0800

    directly takes you to homepage

--- project/project/urls.py
+++ project/project/urls.py
@@ -2,8 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 from .import views
+import re
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', views.home),
+    path("", views.homepage),
 ]
--- project/project/views.py
+++ project/project/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("home")
+
+def homepage(request):
+    return HttpResponse("homepage")

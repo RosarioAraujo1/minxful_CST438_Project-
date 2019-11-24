7f55a84bff08ad10cc79ff085d363abd240d4913
na3la <tm.sc@protonmail.com>
2019-11-24 14:23:21 -0800

    fixing branches

--- minxful/minxful_app/views.py
+++ minxful/minxful_app/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Hello world")
+def userdash(request):
+    return render(request, 'userdash.html')
+
+
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def forum(request):
+    return render(request, 'forum.html')
+
+
+def admindash(request):
+    return render(request, 'admin_dash.html')
+
+def signup(request):
+    return render(request, 'signup.html')

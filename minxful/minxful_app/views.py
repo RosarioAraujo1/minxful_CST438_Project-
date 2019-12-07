b2420639dc5fdba0d86c6aff9b44634c0131ed16
Pernille Dahl <pdahl@csumb.edu>
2019-12-06 23:47:36 -0800

    fixed views

--- minxful/minxful_app/views.py
+++ minxful/minxful_app/views.py
@@ -11,25 +11,12 @@ def userdash(request):
 def homepage(request):
     posts = Post.objects.all().order_by('pub_date')
     return render(request, 'homepage.html', {'posts': posts})
-<<<<<<< HEAD
-# def homepage(request):
-#     return render(request, 'homepage.html', {'posts': posts})
-
-=======
->>>>>>> master
-
 
 def login(request):
     return render(request, 'login.html')
 
-
 def forum(request):
-<<<<<<< HEAD
-=======
-
->>>>>>> master
     return render(request, 'forum.html')
-
 
 def admindash(request):
     return render(request, 'admin_dash.html')

4135fcef83e89f2bedd10c3fb6781dbb40a9cd6c
Rosario <Rosario@Rosario-MacBook-Pro.local>
2019-12-08 12:49:18 -0800

    login working

--- minxful/user_account/urls.py
+++ minxful/user_account/urls.py
@@ -6,5 +6,6 @@ from django.urls import re_path
 app_name = 'user_account'
 
 urlpatterns =[
-    re_path(r'^signup/$', views.signup_view, name="signup")
+    re_path(r'^signup/$', views.signup_view, name="signup"),
+    re_path(r'^login/$', views.login_view, name="login"),
 ]
--- minxful/user_account/views.py
+++ minxful/user_account/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 # Create your views here.
@@ -13,3 +13,12 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, 'user_account/signup.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm()
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'user_account/login.html', {'form': form})

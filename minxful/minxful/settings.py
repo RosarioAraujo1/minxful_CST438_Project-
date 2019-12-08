05f1bc54a8f2c986b020867544eeb90af88a0080
Rosario <Rosario@Rosario-MacBook-Pro.local>
2019-12-07 22:34:33 -0800

    saving users onto database

--- minxful/minxful/settings.py
+++ minxful/minxful/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'user_account'
+    'user_account',
 
     #'crispy_forms',
 ]

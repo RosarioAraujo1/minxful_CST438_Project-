6011f9a47efdd06e3456c12ced27037446b68968
na3la <tm.sc@protonmail.com>
2019-12-09 20:08:33 -0800

    removed .org files generated from vimdiff (urls/views)

--- minxful/user_account/urls.py
+++ minxful/user_account/urls.py
@@ -17,10 +17,4 @@ urlpatterns = [
         include(
             "minxful_app.urls",
             namespace="minxful_app")),
-app_name = 'user_account'
-
-urlpatterns = [
-    re_path(r'^signup/$', views.signup_view, name="signup"),
-    re_path(r'^login/$', views.login_view, name="login"),
-    re_path(r'^logout/$', views.logout_view, name="logout"),
-]
+        ]

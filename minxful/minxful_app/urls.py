12ff157416be5e100c6c5f521016da88391f2569
na3la <tm.sc@protonmail.com>
2019-12-12 20:19:13 -0800

    working almost

--- minxful/minxful_app/urls.py
+++ minxful/minxful_app/urls.py
@@ -1,11 +1,30 @@
-from django.urls import path, re_path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib.auth.views import LogoutView
 from django.conf import settings
+from django.contrib import admin
+from django.urls import path, include, re_path
+# from minxful_app import views
+from django.conf.urls import url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
 
-app_name = "minxful_app"
 urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("userdash/", views.userdash),
+    re_path(r"^$", views.homepage),
+    path("login", views.login),
+    path("forum", views.forum),
+    path("admindash", views.admindash),
+    path("signup", views.signup),
+    url(r"^user_account/", include("user_account.urls")),
+    url(
+        r"^logout/$",
+        LogoutView.as_view(),
+        {"next_page": settings.LOGOUT_REDIRECT_URL},
+        name="logout",
+    ),
     path("", views.homepage, name="homepage"),
     re_path(
         r"^logout/$",
@@ -14,4 +33,7 @@ urlpatterns = [
         name="logout",
     ),
 ]
+
+
 urlpatterns += staticfiles_urlpatterns()
+app_name = "minxful_app"
